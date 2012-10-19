7e834265aff32ce09135fe7d4495a85bd81dce8e
arunkumar339 <arunkumar339@gmail.com>
2012-10-19 23:28:20 +0530

    render text to sms controller

---
+++ app/controllers/sms_controller.rb
@@ -0,0 +1,27 @@
+class SmsController < ApplicationController
+  
+  def main
+    @msg=params[:message]
+    @mobile_number=params[:mobile]
+    @splitted_msg=@msg.split(/\W+/)
+    case @splitted_msg[0].downcase
+    when "register"
+        pass=rand(1000.9999)
+        @user=User.new
+        @user.mobile=params[:mobile]
+        @user.password=pass
+        @user.password_confirmation=pass
+        @user.save
+        @message="your password is #{pass}"
+    when "send"
+        @sms=Sms.new
+        @sms.from=params[:mobile]
+        @sms.to=@splitted_msg[1]
+#        Digest::MD5.hexdigest('foobar')
+        @sms.message=@splitted_msg[2]
+        @sms.save
+    end
+    render :text => "text to render..."
+  end
+
+end

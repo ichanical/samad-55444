ef58f34a04052a7896c0ee654a402eb4f779df47
arunkumar339 <arunkumar339@gmail.com>
2012-10-20 01:01:07 +0530

    user login  enabled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,13 @@ class UsersController < ApplicationController
     if @user.save  
       #   flash[:notice] = "Registration successful. check email for activation" 
 #      redirect_to new_restaurant_path and return
-      raise @user.to_yaml
+     redirect_to user_path(@user)
     else  
       render :action => 'new'  
     end  
   end 
+  def show
+    @user=User.find(params[:id])
+    @messages=Sms.all(:conditions=>[:to=>@user.mobile])
+  end
 end

67f71c2d1181819612e90190ea9fbc57867d929f
arunkumar339 <arunkumar339@gmail.com>
2012-10-20 00:10:23 +0530

    check User already registered

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -7,9 +7,8 @@ class SmsController < ApplicationController
     case @splitted_msg[0].downcase
     when "register"
         pass=rand(100000.999999)
-        user=User.find_by_mobile(:params[:mobile])
-        if user.nil?
-        @user=User.new
+        @user=User.find_by_mobile(params[:mobile])||User.new
+        if @user.new_record?
         @user.mobile=params[:mobile]
         @user.password=pass
         @user.password_confirmation=pass
@@ -17,6 +16,7 @@ class SmsController < ApplicationController
         @message="your password is #{pass}"
         else
          @message="already registered"
+
         end
     when "send"
         @sms=Sms.new
